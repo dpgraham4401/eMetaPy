ed7d9171c78b941cf9eccfce61e7bbf47664327a
dpgraham4401 <dpgraham4401@gmail.com>
2021-05-31 21:36:12 -0400

    preliminary request query results

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ Test use of the MetaBase API
 '''
 
 import sys
-import auth
-import user
 import json
+import auth
+#import user
+import query
 
 if not sys.version_info > (2, 7):
     print('error: python version 2 not supported')
@@ -14,5 +15,5 @@ elif not sys.version_info >= (3, 5):
     print('warning: Python >= 3.5 not supported, proceeding')
 
 auth.token()
-test = user.recent()
+test = query.get_card_id('2991', 'json')
 print(json.dumps(test, indent = 1))
--- query.py
+++ query.py
@@ -2,16 +2,30 @@
 Request qeury  results?
 """
 
-import requests, json, os
+import json
+import os
+import requests
 
 def get_cards(card_type):
     """Request query (card) options"""
     base_url = 'https://rcraquery.epa.gov/metabase'
     auth_url = base_url + '/api/card'
-    id = os.getenv('META_TOKEN')
+    token_id = os.getenv('META_TOKEN')
     meta_head = {'Content-Type': 'application/json',
-                 'X-Metabase-Session': id,
+                 'X-Metabase-Session': token_id,
                  'f': card_type}
     res = requests.get(auth_url, headers=meta_head)
-    data = res.json()
-    return data
+    res = res.json()
+    return res
+
+def get_card_id(card_id, export_frmt):
+    """GET metabase card"""
+    base_url = 'https://rcraquery.epa.gov/metabase'
+    auth_url = base_url + '/api/card/' + card_id + '/query/' + export_frmt
+    print(auth_url)
+    token_id = os.getenv('META_TOKEN')
+    meta_head = {'Content-Type': 'application/json',
+                 'X-Metabase-Session': token_id}
+    res = requests.post(auth_url, headers=meta_head)
+    res = res.json()
+    return res
